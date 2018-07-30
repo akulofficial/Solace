1483683e0906c2bb9da4759e66f401e3c3ecc499
Akul Gulrajani <gulrajani.2@osu.edu>
2018-07-29 22:31:44 -0400

    Initial Commit

---
+++ Solace/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Solace
+//
+//  Created by Akul Gulrajani on 7/29/18.
+//  Copyright © 2018 Akul Gulrajani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
