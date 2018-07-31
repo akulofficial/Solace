72181bf1f949ddcbb0ba228bbf1e90f93c604f40
Akul Gulrajani <gulrajani.2@osu.edu>
2018-07-31 00:37:56 -0400

    Added location manager and location user permissions

--- Solace/ViewController.swift
+++ Solace/ViewController.swift
@@ -7,19 +7,68 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet var Map: MKMapView!
+    var locationManager: CLLocationManager!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager = CLLocationManager()
+        enableBasicLocationServices()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func enableMyWhenInUseFeatures() {
+        //nothing yet
+    }
 
-
+    func disableMyLocationBasedFeatures() {
+        //nothing yet
+    }
+    
+    func enableBasicLocationServices() {
+        locationManager.delegate = self
+        
+        switch CLLocationManager.authorizationStatus() {
+        case .notDetermined:
+            // Request when-in-use authorization initially
+            locationManager.requestWhenInUseAuthorization()
+            break
+            
+        case .restricted, .denied:
+            // Disable location features
+            disableMyLocationBasedFeatures()
+            break
+            
+        case .authorizedWhenInUse, .authorizedAlways:
+            // Enable location features
+            enableMyWhenInUseFeatures()
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .restricted, .denied:
+            disableMyLocationBasedFeatures()
+            break
+            
+        case .authorizedWhenInUse:
+            enableMyWhenInUseFeatures()
+            break
+            
+        case .notDetermined, .authorizedAlways:
+            break
+        }
+    }
+    
 }
 
